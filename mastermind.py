5e0f456f870f4acc4a48d3d2fc63f77da614db66
Piotr Loos <45639299+piotrloos@users.noreply.github.com>
2019-11-26 13:31:40 +0100

    Improved code check and tested, added some comments

--- mastermind.py
+++ mastermind.py
@@ -1,26 +1,37 @@
 from random import randint
 
-PEGS = 4
+###
+# My version of famous game Mastermind
+###
+
+PEGS = 4    # defining how many pegs will be in the game
+COLORS = 6  # defining how many colors will be in the game
 
 
 class Game:
+    """ Game class contains whole game, the pattern and the tries"""
     def __init__(self, code=None):
+        # check if code is given, if not -> randomize new set of pegs
         if code is None:
-            self.pattern = [randint(1, 6) for _ in range(PEGS)]
+            self.pattern = [randint(1, COLORS) for _ in range(PEGS)]
         else:
-            if not isinstance(code, list) or len(code) != PEGS:
+            # check if given code is correct
+            if not isinstance(code, list) or len(code) != PEGS or any(not isinstance(peg, int) for peg in code) or any(peg < 1 or peg > COLORS for peg in code):
                 raise ValueError("Incorrect code.")
             self.pattern = code
+        self.tries = {}  # initialize set of tries
+        self.reveal()  # reveal pattern after successful creation Game class object
 
     def reveal(self):
+        """ Method for printing pattern """
         print(self.pattern)
 
 
+# some tests
+
 a = Game()
-a.reveal()
 
 b = Game([1, 2, 3, 4])
-b.reveal()
 
 try:
     c = Game(2)
@@ -31,3 +42,18 @@ try:
     d = Game([1, 2, 3])
 except ValueError:
     print("d error")
+
+try:
+    e = Game([6, 6, 5, 5])
+except ValueError:
+    print("e error")
+
+try:
+    f = Game([6, 6, 5.4, 5])
+except ValueError:
+    print("f error")
+
+try:
+    g = Game([6, 6, "a", 5])
+except ValueError:
+    print("g error")
