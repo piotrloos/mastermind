5b51db93b83daa1bf15b74397227d9e4c5ef5d95
Piotr Loos <45639299+piotrloos@users.noreply.github.com>
2019-11-27 23:35:13 +0100

    Added calculate_pattern method and few tests for it

--- mastermind.py
+++ mastermind.py
@@ -9,8 +9,10 @@ from settings import PEGS, COLORS
 
 
 class Game:
-    """ Game class contains whole game, the pattern and the tries """
-    def __init__(self, pattern=None, pegs=PEGS, colors=COLORS):
+    """ Game class contains whole game, the solution pattern and the guesses """
+
+    def __init__(self, solution_pattern=None, pegs=PEGS, colors=COLORS):
+        """ Method for preparing new game with given settings """
 
         # check if given pegs number is correct
         if pegs in range(2, 9):  # form 2 to 8
@@ -24,23 +26,57 @@ class Game:
         else:
             raise ValueError("Incorrect number of colors.")
 
-        # check if pattern is given, if not -> randomize new pattern
-        if pattern is None:
-            self.pattern = tuple(randint(1, self.colors) for _ in range(self.pegs))
+        # check if solution_pattern is given, if not -> randomize new pattern
+        if solution_pattern is None:
+            self.solution_pattern = tuple(randint(1, self.colors) for _ in range(self.pegs))
         else:
-            # check if given pattern is correct
-            if self.validate_pattern(pattern):
-                self.pattern = pattern
+            # check if given solution_pattern is correct
+            if self.validate_pattern(solution_pattern):
+                self.solution_pattern = solution_pattern
             else:
-                raise ValueError("Incorrect pattern.")
+                raise ValueError("Incorrect solution pattern.")
 
-        self.guesses = {}  # initialize dict of guesses
-        print(self.reveal_pattern())  # reveal pattern after successful creation Game class object
+        self.guess_dict = {}  # initialize dictionary of guesses
+        self.guess_count = 0  # initialize guess counter
+        self.game_finished = False  # initialize flag which indicates whether pattern is guessed
+        # print(self.reveal_pattern())  # reveal pattern after successful creation Game class object
 
     def validate_pattern(self, pattern):
+        """ Method for validating given pattern """
+
         return isinstance(pattern, tuple) and len(pattern) == self.pegs and all(
             peg in range(1, self.colors + 1) for peg in pattern)
 
+    def calculate_pattern(self, pattern):
+        """ Method for calculating black and white pegs from guess pattern """
+
+        # black_pegs defines how many guess pegs are in appropriate color and in appropriate location
+        black_pegs = sum(1 for guess_peg, solution_peg in zip(pattern, self.solution_pattern) if guess_peg == solution_peg)
+
+        # white_black_pegs defines how many guess pegs are in appropriate color regardless to location
+        # white_pegs defines how many guess pegs are in appropriate color and unappropriate location
+        # to calculate white_pegs you must subtract black_pegs from white_black_pegs
+        white_black_pegs = sum(min(pattern.count(peg), self.solution_pattern.count(peg)) for peg in range(1, self.colors + 1))
+
+        return black_pegs, white_black_pegs - black_pegs  # return tuple with black and white pegs
+
+    def guess_pattern(self, pattern):
+        """ Method for guessing pattern by the player """
+
+        if not self.validate_pattern(pattern):  # check if given pattern is correct
+            raise ValueError("Incorrect guess pattern.")
+
+        self.guess_count += 1  # increment the guess counter
+
+        if pattern not in self.guess_dict:  # check if given pattern was already calculated
+            self.guess_dict[pattern] = self.calculate_pattern(pattern)  # save the result to the dictionary
+            if self.guess_dict[pattern] == (self.pegs, 0):  # check if the pattern is guessed correctly
+                self.game_finished = True
+
+        return self.guess_dict[pattern]
+
     def reveal_pattern(self):
-        """ Method for returning pattern """
-        return self.pattern
+        """ Method for returning solution pattern """
+
+        self.game_finished = True
+        return self.solution_pattern
--- tests.py
+++ tests.py
@@ -9,15 +9,20 @@ from mastermind import Game
 
 
 class GameCreationTests(unittest.TestCase):
-    """ Testing class for creations new game """
+    """ Testing class for creations new game with standard settings """
+
     def test_new_game_with_correct_pattern(self):
         pattern = (1, 2, 3, 4)
         a = Game(pattern)
+        self.assertFalse(a.game_finished)
         self.assertTupleEqual(a.reveal_pattern(), pattern)
+        self.assertTrue(a.game_finished)
 
     def test_new_game_without_given_pattern(self):
         a = Game()
+        self.assertFalse(a.game_finished)
         self.assertTrue(a.validate_pattern(a.reveal_pattern()))
+        self.assertTrue(a.game_finished)
 
     def test_new_game_with_incorrect_pattern1(self):
         with self.assertRaises(ValueError):
@@ -38,3 +43,78 @@ class GameCreationTests(unittest.TestCase):
     def test_new_game_with_incorrect_pattern5(self):
         with self.assertRaises(ValueError):
             Game((6, 6, "a", 5))
+
+
+class CustomGameCreationTests(unittest.TestCase):
+    """ Testing class for creations new game with custom settings """
+
+    def test_new_game_with_more_pegs_number(self):
+        pattern = (1, 2, 3, 4, 5)
+        a = Game(pattern, pegs=5)
+        self.assertTupleEqual(a.reveal_pattern(), pattern)
+
+    def test_new_game_with_less_pegs_number(self):
+        pattern = (1, 2)
+        a = Game(pattern, pegs=2)
+        self.assertTupleEqual(a.reveal_pattern(), pattern)
+
+    def test_new_game_with_incorrect_pegs_number(self):
+        pattern = (1,)
+        with self.assertRaises(ValueError):
+            Game(pattern, pegs=1)
+
+    def test_new_game_with_more_colors_number(self):
+        pattern = (1, 2, 7, 8)
+        a = Game(pattern, colors=8)
+        self.assertTupleEqual(a.reveal_pattern(), pattern)
+
+    def test_new_game_with_less_colors_number(self):
+        pattern = (1, 2, 1, 2)
+        a = Game(pattern, colors=2)
+        self.assertTupleEqual(a.reveal_pattern(), pattern)
+
+    def test_new_game_with_incorrect_colors_number(self):
+        pattern = (1, 1, 1, 1)
+        with self.assertRaises(ValueError):
+            Game(pattern, colors=1)
+
+    def test_new_game_with_custom_correct_settings(self):
+        pattern = (8, 4, 1, 3, 8, 5)
+        a = Game(pattern, pegs=6, colors=8)
+        self.assertTupleEqual(a.reveal_pattern(), pattern)
+
+    def test_new_game_with_custom_incorrect_settings1(self):
+        pattern = (4, 7, 5, 2, 8)
+        with self.assertRaises(ValueError):
+            Game(pattern, pegs=7, colors=8)
+
+    def test_new_game_with_custom_incorrect_settings2(self):
+        pattern = (4, 7, 5, 2, 8)
+        with self.assertRaises(ValueError):
+            Game(pattern, pegs=5, colors=7)
+
+    def test_new_game_with_custom_incorrect_settings3(self):
+        pattern = (4, 7, 5, 2, 8)
+        with self.assertRaises(ValueError):
+            Game(pattern, pegs=3, colors=4)
+
+
+class PegsCalculateTests(unittest.TestCase):
+    """ Testing class for calculate black and white pegs """
+
+    def setUp(self):
+        self.a = Game(solution_pattern=(2, 8, 8, 3, 5, 3), pegs=6, colors=10)
+
+    def test_example_game(self):
+        self.assertTupleEqual(self.a.guess_pattern((1, 6, 5, 6, 8, 4)), (0, 2))
+        self.assertTupleEqual(self.a.guess_pattern((9, 2, 3, 7, 2, 10)), (0, 2))
+        self.assertTupleEqual(self.a.guess_pattern((8, 8, 10, 5, 10, 3)), (2, 2))
+        self.assertTupleEqual(self.a.guess_pattern((3, 8, 10, 10, 1, 5)), (1, 2))
+        self.assertTupleEqual(self.a.guess_pattern((5, 4, 10, 8, 3, 8)), (0, 4))
+        self.assertTupleEqual(self.a.guess_pattern((10, 5, 8, 9, 5, 8)), (2, 1))
+        self.assertTupleEqual(self.a.guess_pattern((7, 7, 8, 3, 8, 5)), (2, 2))
+        self.assertTupleEqual(self.a.guess_pattern((2, 8, 6, 3, 5, 3)), (5, 0))
+        self.assertTupleEqual(self.a.guess_pattern((8, 3, 2, 5, 3, 8)), (0, 6))
+        self.assertFalse(self.a.game_finished)
+        self.assertTupleEqual(self.a.guess_pattern((2, 8, 8, 3, 5, 3)), (6, 0))
+        self.assertTrue(self.a.game_finished)
