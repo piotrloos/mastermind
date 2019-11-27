f12dcaccbe47528ddf24160747a59a0b26cbf247
Piotr Loos <45639299+piotrloos@users.noreply.github.com>
2019-11-27 12:17:46 +0100

    Customize pegs and colors number, minor changes

--- mastermind.py
+++ mastermind.py
@@ -1,8 +1,8 @@
-###
-# My version of famous game Mastermind
-# mastermind.py
-# Main file
-###
+########################################
+# My version of famous game Mastermind #
+# mastermind.py                        #
+# Main file                            #
+########################################
 
 from random import randint
 from settings import PEGS, COLORS
@@ -10,18 +10,37 @@ from settings import PEGS, COLORS
 
 class Game:
     """ Game class contains whole game, the pattern and the tries """
-    def __init__(self, code=None):
-        # check if code is given, if not -> randomize new set of pegs
-        if code is None:
-            self.pattern = (randint(1, COLORS) for _ in range(PEGS))
-        else:
-            # check if given code is correct
-            if not isinstance(code, tuple) or len(code) != PEGS or any(not isinstance(peg, int) or peg < 1 or peg > COLORS for peg in code):
-                raise ValueError("Incorrect code.")
-            self.pattern = code
-        self.tries = {}  # initialize dict of tries
-        self.reveal()  # reveal pattern after successful creation Game class object
+    def __init__(self, pattern=None, pegs=PEGS, colors=COLORS):
 
-    def reveal(self):
+        # check if given pegs number is correct
+        if pegs in range(2, 9):  # form 2 to 8
+            self.pegs = pegs
+        else:
+            raise ValueError("Incorrect number of pegs.")
+
+        # check if given colors number is correct
+        if colors in range(2, 11):  # from 2 to 10
+            self.colors = colors
+        else:
+            raise ValueError("Incorrect number of colors.")
+
+        # check if pattern is given, if not -> randomize new pattern
+        if pattern is None:
+            self.pattern = tuple(randint(1, self.colors) for _ in range(self.pegs))
+        else:
+            # check if given pattern is correct
+            if self.validate_pattern(pattern):
+                self.pattern = pattern
+            else:
+                raise ValueError("Incorrect pattern.")
+
+        self.guesses = {}  # initialize dict of guesses
+        print(self.reveal_pattern())  # reveal pattern after successful creation Game class object
+
+    def validate_pattern(self, pattern):
+        return isinstance(pattern, tuple) and len(pattern) == self.pegs and all(
+            peg in range(1, self.colors + 1) for peg in pattern)
+
+    def reveal_pattern(self):
         """ Method for returning pattern """
         return self.pattern
--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
-###
-# My version of famous game Mastermind
-# tests.py
-# Unittests for mastermind
-###
+########################################
+# My version of famous game Mastermind #
+# tests.py                             #
+# Unittests for mastermind             #
+########################################
 
 import unittest
 from mastermind import Game
@@ -10,31 +10,31 @@ from mastermind import Game
 
 class GameCreationTests(unittest.TestCase):
     """ Testing class for creations new game """
-    def test_new_game_with_correct_code(self):
-        code = (1, 2, 3, 4)
-        a = Game(code)
-        self.assertTupleEqual(a.reveal(), code)
+    def test_new_game_with_correct_pattern(self):
+        pattern = (1, 2, 3, 4)
+        a = Game(pattern)
+        self.assertTupleEqual(a.reveal_pattern(), pattern)
 
-    def test_new_game_without_given_code(self):
-        Game()
-        self.assertTrue(True)
+    def test_new_game_without_given_pattern(self):
+        a = Game()
+        self.assertTrue(a.validate_pattern(a.reveal_pattern()))
 
-    def test_new_game_with_incorrect_code1(self):
+    def test_new_game_with_incorrect_pattern1(self):
         with self.assertRaises(ValueError):
             Game(2)
 
-    def test_new_game_with_incorrect_code2(self):
+    def test_new_game_with_incorrect_pattern2(self):
         with self.assertRaises(ValueError):
             Game((1, 2, 3))
 
-    def test_new_game_with_incorrect_code3(self):
+    def test_new_game_with_incorrect_pattern3(self):
         with self.assertRaises(ValueError):
             Game((6, 6, 5, 7))
 
-    def test_new_game_with_incorrect_code4(self):
+    def test_new_game_with_incorrect_pattern4(self):
         with self.assertRaises(ValueError):
             Game((6, 6, 5.4, 5))
 
-    def test_new_game_with_incorrect_code5(self):
+    def test_new_game_with_incorrect_pattern5(self):
         with self.assertRaises(ValueError):
             Game((6, 6, "a", 5))
